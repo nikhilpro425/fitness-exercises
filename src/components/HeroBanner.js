1b07f36d983b3dc8d0b6097466dc6a46ff95af0a
nikhilpro425 <nikhilthebest2005@gmail.com>
2024-06-21 02:21:46 +0530

    Complete HeroBanner component of HomePage

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import { Box, Stack, Typography, Button } from '@mui/material'
+
+import HeroBannerImage from '../assets/images/banner.png'
+import { Padding } from '@mui/icons-material'
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{
+        mt: {lg: '212px', xs: '70px'},
+        ml: {sm: '50px'}
+    }} position='relative' p='20'>
+        <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+            FITNESS CLUB
+        </Typography>
+        <Typography fontWeight='700' fontSize='40px' mb='23px' mt='30px'>
+            Sweat, Smile <br /> and REPEAT
+        </Typography>
+        <Typography fontSize='22px' lineHeight='35px' mb={2}>
+            Checkout the most effective exercises
+        </Typography>
+        <Button variant='contained' color='error' href='#exercises'
+            sx={{ backgroundColor: "#FF2625", padding: '10px'
+            }}
+
+        >
+            Explore Exercises</Button>
+        <Typography
+            fontWeight='600'
+            color="#FF2625"
+            sx={{
+                opacity: '0.1',
+                display: { lg: 'block', xs: 'none'}
+            }}
+            fontSize='200px'
+        >
+            EXERCISES
+        </Typography>
+        <img src={HeroBannerImage} alt='banner' className='hero-banner-img' />
+    </Box>
   )
 }
 
