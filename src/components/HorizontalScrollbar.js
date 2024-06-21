d48ac7842d16c388b5cdab21202f240220852454
nikhilpro425 <nikhilthebest2005@gmail.com>
2024-06-22 02:00:02 +0530

    Minor code refactoring and beautification

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { Box, Typography } from '@mui/material'
-import {ScrollMenu, Visibility, VisibilityContext} from 'react-horizontal-scrolling-menu'
+import {ScrollMenu, VisibilityContext} from 'react-horizontal-scrolling-menu'
 
 import BodyPart from './BodyPart'
 import RightArrowIcon from '../assets/icons/right-arrow.png'
